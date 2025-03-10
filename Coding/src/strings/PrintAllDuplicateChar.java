c519b1d5f94e1b64d70af3616cba47c21914a443
somu <suryasomasundar@gmail.com>
2025-03-10 12:25:39 -0700

    Strings - Update Main Function

--- Coding/src/strings/PrintAllDuplicateChar.java
+++ Coding/src/strings/PrintAllDuplicateChar.java
@@ -7,6 +7,10 @@ public class PrintAllDuplicateChar {
 
     public static void printDuplicates(String str) {
 
+        if (str == null || str.isEmpty()) {
+            System.out.println("The String is Empty");
+        }
+
         HashMap<Character, Integer> map = new HashMap<>();
 
         for (char c : str.toCharArray()) {
@@ -22,7 +26,15 @@ public class PrintAllDuplicateChar {
     }
 
     public static void main(String[] args) {
-        String str = "aabbccd";
-        printDuplicates(str);
+        String str1 = "HelloWorld";
+        String str2 = "aabbccdd";
+        String str3 = "";
+        String str4 = "a";
+        String str5 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbc";
+        printDuplicates(str1);
+        printDuplicates(str2);
+        printDuplicates(str3);
+        printDuplicates(str4);
+        printDuplicates(str5);
     }
 }
